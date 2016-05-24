0a6e7f8cfd3b5490da530f79fbc4348d52df0f1d
mxmlnkn <mxmlnkn@github.de>
2016-05-24 21:21:37 +0200

    noticed weird place which screams to be a bug ...

--- src/org/trifort/rootbeer/generate/bytecode/StaticOffsets.java
+++ src/org/trifort/rootbeer/generate/bytecode/StaticOffsets.java
@@ -29,6 +29,9 @@ import soot.rbclassload.RootbeerClassLoader;
 
 public class StaticOffsets
 {
+    /* if m_offsetToFieldMap was only the inverse map to m_fieldToOffsetMap
+     * e.g. for performance reasons, then why does one have OpenCLField
+     * while the other uses SortableField ?? */
     private final Map<Integer    , SortableField     > m_offsetToFieldMap;
     private final Map<OpenCLField, Integer           > m_fieldToOffsetMap;
     private final Map<SootClass  , Integer           > m_classToOffsetMap;
@@ -63,63 +66,78 @@ public class StaticOffsets
         return ret;
     }
 
-  private void buildMaps() {
-    List<CompositeField> composites = OpenCLScene.v().getCompositeFields();
-    Set<SortableField> sortable_fields = new HashSet<SortableField>();
-    for(CompositeField composite : composites){
-      List<SootClass> classes = composite.getClasses();
-      for(SootClass soot_class : classes){
-        sortable_fields.addAll(convert(composite.getRefFieldsByClass(soot_class), soot_class));
-        sortable_fields.addAll(convert(composite.getNonRefFieldsByClass(soot_class), soot_class));
+    /**
+     * Collects all classes and saves their corresponding address in a map
+     * Note: I think this is similar to what `nm -C` shows, i.e. to one
+     *       object file / class a list of functions and variables.
+     */
+    private void buildMaps()
+    {
+        final List<CompositeField> composites = OpenCLScene.v().getCompositeFields();
+        final Set <SortableField > sortable_fields = new HashSet<SortableField>();
+        for ( final CompositeField composite : composites )
+        {
+            for ( final SootClass soot_class : composite.getClasses() )
+            {
+                final List<OpenCLField> refs    = composite.getRefFieldsByClass   (soot_class);
+                final List<OpenCLField> nonrefs = composite.getNonRefFieldsByClass(soot_class);
 
-        List<OpenCLField> static_fields = new ArrayList<OpenCLField>();
-        static_fields.addAll(staticFilter(composite.getRefFieldsByClass(soot_class)));
-        static_fields.addAll(staticFilter(composite.getNonRefFieldsByClass(soot_class)));
-        m_staticFields.put(soot_class, static_fields);
-      }
-    }
-    SortableField[] array = new SortableField[sortable_fields.size()];
-    array = sortable_fields.toArray(array);
-    Arrays.sort(array);
-    int index = 0;
-    for(SortableField field : array){
-      m_offsetToFieldMap.put(index, field);
-      m_fieldToOffsetMap.put(field.m_field, index);
-      int size = field.m_field.getSize();
-      index += size;
-    }
-    int leftover = index % 4;
-    if(leftover != 0){
-      index += leftover;
-    }
-    m_lockStart = index;
-    Set<Type> types = RootbeerClassLoader.v().getDfsInfo().getDfsTypes();
-    for(Type type : types){
-      if(type instanceof RefType == false){
-        continue;
-      }
-      RefType ref_type = (RefType) type;
-      String curr = ref_type.getClassName();
-      SootClass soot_class = Scene.v().getSootClass(curr);
-      m_classToOffsetMap.put(soot_class, index);
-      index += 4;
-    }
-    m_endIndex = index;
-    int mod = m_endIndex % 16;
-    m_zerosSize = 0;
-    if(mod != 0){
-      m_endIndex += (16 - mod);
-      m_zerosSize += (16 - mod);
-    }
-    //give room for junk space. some reason 32 bytes of space are needed for
-    //the synchronized tests to pass.
-    m_endIndex += 32;
-    m_zerosSize += 32;
-  }
+                sortable_fields.addAll( convert( refs   , soot_class) );
+                sortable_fields.addAll( convert( nonrefs, soot_class) );
 
-  public int getZerosSize(){
-    return m_zerosSize;
-  }
+                final List<OpenCLField> static_fields = new ArrayList<OpenCLField>();
+                static_fields.addAll( staticFilter( refs    ) );
+                static_fields.addAll( staticFilter( nonrefs ) );
+
+                m_staticFields.put( soot_class, static_fields );
+            }
+        }
+        /* why is a new object being created if it is overwritten by toArray
+         * right after that anyway ?? */
+        SortableField[] array = new SortableField[ sortable_fields.size() ];
+        array = sortable_fields.toArray(array);
+        Arrays.sort( array );
+
+        int index = 0;
+        for ( final SortableField field : array )
+        {
+            m_offsetToFieldMap.put( index, field );
+            m_fieldToOffsetMap.put( field.m_field, index );
+            index += field.m_field.getSize();
+        }
+        /* Align to 4 (why? I don't think these are bytes, so why the need to
+         * align?) Actually ... this isn't alignment... What is this oO?
+         * Note also that +=0 does do nothing, so no need for if ... */
+        int leftover = index % 4;
+        if ( leftover != 0 ) {
+            index += leftover;
+        }
+        m_lockStart = index;
+        Set<Type> types = RootbeerClassLoader.v().getDfsInfo().getDfsTypes();
+        for(Type type : types){
+          if(type instanceof RefType == false){
+            continue;
+          }
+          RefType ref_type = (RefType) type;
+          String curr = ref_type.getClassName();
+          SootClass soot_class = Scene.v().getSootClass(curr);
+          m_classToOffsetMap.put(soot_class, index);
+          index += 4;
+        }
+        m_endIndex = index;
+        int mod = m_endIndex % 16;
+        m_zerosSize = 0;
+        if(mod != 0){
+          m_endIndex += (16 - mod);
+          m_zerosSize += (16 - mod);
+        }
+        //give room for junk space. some reason 32 bytes of space are needed for
+        //the synchronized tests to pass.
+        m_endIndex  += 32;
+        m_zerosSize += 32;
+    }
+
+    public int getZerosSize(){ return m_zerosSize; }
 
   private List<SortableField> convert(List<OpenCLField> fields, SootClass soot_class) {
     fields = staticFilter(fields);
