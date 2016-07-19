c6e7aa2f238306664c6e63378650d72dafadb57b
mxmlnkn <mxmlnkn@github.de>
2016-07-19 04:09:16 +0200

    add debug output when copying to GPU

--- csrc/org/trifort/rootbeer/runtime/CUDARuntime.c
+++ csrc/org/trifort/rootbeer/runtime/CUDARuntime.c
@@ -1,134 +1,135 @@
+
 #include "CUDARuntime.h"
 #include <cuda.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <assert.h>
+#include <stdio.h>      // sprintf
+
+
+#define CE( STATUS )                                                \
+{                                                                   \
+    const CUresult status = STATUS;                                 \
+    const char *errorName, *errorString;                            \
+    cuGetErrorName  ( status, &errorName   );                       \
+    cuGetErrorString( status, &errorString );                       \
+    if ( status != CUDA_SUCCESS )                                   \
+    {                                                               \
+        char msg[8*1024];                                           \
+        sprintf( msg,                                               \
+            "Line %i, command: "#STATUS"\n failed with "            \
+            "error code %i (%.1000s) : %.4000s\n",                  \
+            __LINE__, status, errorString, errorString );           \
+        assert( status == CUDA_SUCCESS );                           \
+    }                                                               \
+}
+
 
 /**
  * Basically the same functionality like cudaGetDeviceProperties looped over
  * cudaGetDeviceCount. Not sure why CUDA runtime is used directly. Faster?
  * Basically 95% boiler plate code
+ *
+ * Uses JNI to create an ArrayList[GpuDevice] object
  */
 JNIEXPORT jobject JNICALL
 Java_org_trifort_rootbeer_runtime_CUDARuntime_loadGpuDevices
-( JNIEnv * env, jobject this_ref )
+(
+    JNIEnv * env,
+    jobject this_ref
+)
 {
-    int i;
-    int status;
-    int num_devices;
-    CUdevice device;
-    CUcontext context;
+    jclass    gpu_device_class;
+    jmethodID gpu_device_init ;
+    jobject   gpu_device      ;
 
-    jclass array_list_class;
-    jmethodID array_list_init;
-    jmethodID array_list_add;
-    jobject ret;
-
-    jclass gpu_device_class;
-    jmethodID gpu_device_init;
-    jobject gpu_device;
-
-    int major_version;
-    int minor_version;
-    char device_name[4096];
-    size_t free_mem;
-    size_t total_mem;
-    int registers_per_block;
-    int warp_size;
-    int pitch;
-    int threads_per_block;
-    int shared_mem_per_block;
-    int clock_rate;
-    int mem_clock_rate;
-    int const_mem;
-    int integrated;
-    int threads_per_multiprocessor;
-    int multiprocessor_count;
-    int max_block_dim_x;
-    int max_block_dim_y;
-    int max_block_dim_z;
-    int max_grid_dim_x;
-    int max_grid_dim_y;
-    int max_grid_dim_z;
-
-    array_list_class = (*env)->FindClass(env, "java/util/ArrayList");
-    array_list_init  = (*env)->GetMethodID(env, array_list_class, "<init>", "()V");
-    array_list_add   = (*env)->GetMethodID(env, array_list_class, "add", "(Ljava/lang/Object;)Z");
-
-    ret = (*env)->NewObject(env, array_list_class, array_list_init);
+    jclass    array_list_class = (*env)->FindClass  ( env, "java/util/ArrayList" );
+    jmethodID array_list_init  = (*env)->GetMethodID( env, array_list_class, "<init>", "()V" );
+    jmethodID array_list_add   = (*env)->GetMethodID( env, array_list_class, "add", "(Ljava/lang/Object;)Z" );
+    jobject   ret              = (*env)->NewObject  ( env, array_list_class, array_list_init );
 
     gpu_device_class = (*env)->FindClass(env, "org/trifort/rootbeer/runtime/GpuDevice");
     gpu_device_init  = (*env)->GetStaticMethodID(env, gpu_device_class, "newCudaDevice",
       "(IIILjava/lang/String;JJIIIIIIIIZIIIIIIII)Lorg/trifort/rootbeer/runtime/GpuDevice;");
+                          /* ^ function signature for constructor arguments */
 
-    status = cuInit(0);
-    if(status != CUDA_SUCCESS){
+    int status = cuInit(0);
+    if ( status != CUDA_SUCCESS )
         return ret;
-    }
 
-    cuDeviceGetCount(&num_devices);
+    int nDevices = 0;
+    cuDeviceGetCount( &nDevices );
 
-    for( i = 0; i < num_devices; ++i )
+    int iDevice = 0;
+    for ( iDevice = 0; iDevice < nDevices; ++iDevice )
     {
-        status = cuDeviceGet( &device, i );
-        if ( status != CUDA_SUCCESS ) {
-          continue;
-        }
+        CUdevice device;
+        status = cuDeviceGet( &device, iDevice );
+        if ( status != CUDA_SUCCESS )
+            continue;
 
-        cuDeviceComputeCapability( &major_version, &minor_version, device );
-        cuDeviceGetName( device_name, 4096, device );
-        cuCtxCreate ( &context, CU_CTX_MAP_HOST, device );
-        cuMemGetInfo( &free_mem, &total_mem );
-        cuCtxDestroy( context );
-        cuDeviceGetAttribute( &registers_per_block , CU_DEVICE_ATTRIBUTE_MAX_REGISTERS_PER_BLOCK    , device );
-        cuDeviceGetAttribute( &warp_size           , CU_DEVICE_ATTRIBUTE_WARP_SIZE                  , device );
-        cuDeviceGetAttribute( &pitch               , CU_DEVICE_ATTRIBUTE_MAX_PITCH                  , device );
-        cuDeviceGetAttribute( &threads_per_block   , CU_DEVICE_ATTRIBUTE_MAX_THREADS_PER_BLOCK      , device );
-        cuDeviceGetAttribute( &shared_mem_per_block, CU_DEVICE_ATTRIBUTE_MAX_SHARED_MEMORY_PER_BLOCK, device );
-        cuDeviceGetAttribute( &clock_rate          , CU_DEVICE_ATTRIBUTE_CLOCK_RATE                 , device );
-        cuDeviceGetAttribute( &mem_clock_rate      , CU_DEVICE_ATTRIBUTE_MEMORY_CLOCK_RATE          , device );
-        cuDeviceGetAttribute( &const_mem           , CU_DEVICE_ATTRIBUTE_TOTAL_CONSTANT_MEMORY      , device );
-        cuDeviceGetAttribute( &integrated          , CU_DEVICE_ATTRIBUTE_INTEGRATED                 , device );
-        cuDeviceGetAttribute( &multiprocessor_count, CU_DEVICE_ATTRIBUTE_MULTIPROCESSOR_COUNT       , device );
-        cuDeviceGetAttribute( &max_block_dim_x     , CU_DEVICE_ATTRIBUTE_MAX_BLOCK_DIM_X            , device );
-        cuDeviceGetAttribute( &max_block_dim_y     , CU_DEVICE_ATTRIBUTE_MAX_BLOCK_DIM_Y            , device );
-        cuDeviceGetAttribute( &max_block_dim_z     , CU_DEVICE_ATTRIBUTE_MAX_BLOCK_DIM_Z            , device );
-        cuDeviceGetAttribute( &max_grid_dim_x      , CU_DEVICE_ATTRIBUTE_MAX_GRID_DIM_X             , device );
-        cuDeviceGetAttribute( &max_grid_dim_y      , CU_DEVICE_ATTRIBUTE_MAX_GRID_DIM_Y             , device );
-        cuDeviceGetAttribute( &max_grid_dim_z      , CU_DEVICE_ATTRIBUTE_MAX_GRID_DIM_Z             , device );
-        cuDeviceGetAttribute( &threads_per_multiprocessor, CU_DEVICE_ATTRIBUTE_MAX_THREADS_PER_MULTIPROCESSOR, device );
+        int    major_version    ;
+        int    minor_version    ;
+        char   device_name[4096];
+        size_t free_mem         ;
+        size_t total_mem        ;
+        CUcontext context       ;
+
+        CE( cuDeviceComputeCapability( &major_version, &minor_version, device ) );
+        CE( cuDeviceGetName( device_name, 4096, device ) );
+        CE( cuCtxCreate ( &context, CU_CTX_MAP_HOST, device ) );
+        CE( cuMemGetInfo( &free_mem, &total_mem ) );
+        CE( cuCtxDestroy( context ) );
+
+        /* makes use of https://gcc.gnu.org/onlinedocs/gcc/Statement-Exprs.html
+         * to be able to write the constructor call, variable declaration and
+         * call to cuDeviceGetAttribute in one go using a macro.
+         * Also seems to work with -std=c99 switch:
+         *   f(
+         *       ({ int j = 5; j+1; })
+         *   );
+         */
+        #define CUATTR( NAME )                                      \
+        ( {                                                         \
+            int NAME;                                               \
+            CE( cuDeviceGetAttribute( &NAME,                        \
+                                      CU_DEVICE_ATTRIBUTE_##NAME,   \
+                                      device ) )                    \
+            NAME;                                                   \
+        } )
 
         /* @see GpuDevice.java */
         gpu_device = (*env)->CallObjectMethod
         (
-            env                                   ,
-            gpu_device_class                      ,
-            gpu_device_init                       ,
-            i                                     ,  // device_id
-            major_version                         ,  // major_version
-            minor_version                         ,  // minor_version
-            (*env)->NewStringUTF(env, device_name),  // device_name
-            (jlong) free_mem                      ,  // free_global_mem_size
-            (jlong) total_mem                     ,  // total_global_mem_size
-            registers_per_block                   ,  // max_registers_per_block
-            warp_size                             ,  // warp_size
-            pitch                                 ,  // max_pitch
-            threads_per_block                     ,  // max_threads_per_block
-            shared_mem_per_block                  ,  // max_shared_memory_per_block
-            clock_rate                            ,  // clock_rate
-            mem_clock_rate                        ,  // memory_clock_rate
-            const_mem                             ,  // constant_mem_size
-            integrated                            ,  // integrated
-            threads_per_multiprocessor            ,  // max_threads_per_multiprocessor
-            multiprocessor_count                  ,  // multiprocessor_count
-            max_block_dim_x                       ,  // max_block_dim_x
-            max_block_dim_y                       ,  // max_block_dim_y
-            max_block_dim_z                       ,  // max_block_dim_z
-            max_grid_dim_x                        ,  // max_grid_dim_x
-            max_grid_dim_y                        ,  // max_grid_dim_y
-            max_grid_dim_z                           // max_grid_dim_z
+            env                                     ,
+            gpu_device_class                        ,
+            gpu_device_init                         ,
+            iDevice                                 ,  // device_id
+            major_version                           ,  // major_version
+            minor_version                           ,  // minor_version
+            (*env)->NewStringUTF(env, device_name)  ,  // device_name
+            (jlong) free_mem                        ,  // free_global_mem_size
+            (jlong) total_mem                       ,  // total_global_mem_size
+            CUATTR( MAX_REGISTERS_PER_BLOCK        ),  // max_registers_per_block
+            CUATTR( WARP_SIZE                      ),  // warp_size
+            CUATTR( MAX_PITCH                      ),  // max_pitch
+            CUATTR( MAX_THREADS_PER_BLOCK          ),  // max_threads_per_block
+            CUATTR( MAX_SHARED_MEMORY_PER_BLOCK    ),  // max_shared_memory_per_block
+            CUATTR( CLOCK_RATE                     ),  // clock_rate
+            CUATTR( MEMORY_CLOCK_RATE              ),  // memory_clock_rate
+            CUATTR( TOTAL_CONSTANT_MEMORY          ),  // constant_mem_size
+            CUATTR( INTEGRATED                     ),  // integrated
+            CUATTR( MAX_THREADS_PER_MULTIPROCESSOR ),  // max_threads_per_multiprocessor
+            CUATTR( MULTIPROCESSOR_COUNT           ),  // multiprocessor_count
+            CUATTR( MAX_BLOCK_DIM_X                ),  // max_block_dim_x
+            CUATTR( MAX_BLOCK_DIM_Y                ),  // max_block_dim_y
+            CUATTR( MAX_BLOCK_DIM_Z                ),  // max_block_dim_z
+            CUATTR( MAX_GRID_DIM_X                 ),  // max_grid_dim_x
+            CUATTR( MAX_GRID_DIM_Y                 ),  // max_grid_dim_y
+            CUATTR( MAX_GRID_DIM_Z                 )   // max_grid_dim_z
         );
-        (*env)->CallBooleanMethod(env, ret, array_list_add, gpu_device);
+
+        #undef CUATTR
+
+        (*env)->CallBooleanMethod( env, ret, array_list_add, gpu_device );
     }
 
     return ret;
