99c9e79cdf693af2cb54787eea6ae0dbcc90c71e
mxmlnkn <mxmlnkn@github.de>
2016-07-19 03:31:32 +0200

    also from searching compile error

--- src/org/trifort/rootbeer/generate/opencl/tweaks/CudaTweaks.java
+++ src/org/trifort/rootbeer/generate/opencl/tweaks/CudaTweaks.java
@@ -72,41 +72,50 @@ public class CudaTweaks extends Tweaks {
    *
    * @param cuda_code string containing code.
    * @param compileArch determine if we need to build 32bit, 64bit or both.
-   * @return an array containing compilation results. You can use <tt>is32Bit()</tt> on each element
-   * to determine if it is 32 bit or 64bit code. If compilation for an architecture fails, only the
-   * offending element is returned.
+   * @return an array containing compilation results.
+   *         You can use <tt>is32Bit()</tt> on each element to determine
+   *         if it is 32 bit or 64bit code. If compilation for an architecture
+   *         fails, only the offending element is returned.
    */
-  public CompileResult[] compileProgram(String cuda_code, CompileArchitecture compileArch) {
-    PrintWriter writer;
-    try {
-      writer = new PrintWriter(RootbeerPaths.v().getRootbeerHome() + "pre_dead.cu");
-      writer.println(cuda_code);
-      writer.flush();
-      writer.close();
+    public CompileResult[] compileProgram
+    (
+        String cuda_code,
+        final CompileArchitecture compileArch
+    )
+    {
+        PrintWriter writer;
+        try
+        {
+            writer = new PrintWriter(RootbeerPaths.v().getRootbeerHome() + "pre_dead.cu");
+            writer.println( cuda_code );
+            writer.flush();
+            writer.close();
 
-      DeadMethods dead_methods = new DeadMethods();
-      dead_methods.parseString(cuda_code);
-      cuda_code = dead_methods.getResult();
+            final DeadMethods dead_methods = new DeadMethods();
+            dead_methods.parseString( cuda_code );
+            cuda_code = dead_methods.getResult();
 
-      //Compressor compressor = new Compressor();
-      //cuda_code = compressor.compress(cuda_code);
+            //Compressor compressor = new Compressor();
+            //cuda_code = compressor.compress(cuda_code);
 
-      File generated = new File(RootbeerPaths.v().getRootbeerHome() + "generated.cu");
-      writer = new PrintWriter(generated);
-      writer.println(cuda_code.toString());
-      writer.flush();
-      writer.close();
+            File generated = new File( RootbeerPaths.v().getRootbeerHome() + "generated.cu" );
+            writer = new PrintWriter(generated);
+            writer.println(cuda_code.toString());
+            writer.flush();
+            writer.close();
 
-      CudaPath cuda_path = new CudaPath();
-      GencodeOptions options_gen = new GencodeOptions();
-      String gencode_options = options_gen.getOptions();
+            CudaPath cuda_path = new CudaPath();
+            GencodeOptions options_gen = new GencodeOptions();
+            String gencode_options = options_gen.getOptions();
 
-      ParallelCompile parallel_compile = new ParallelCompile();
-      return parallel_compile.compile(generated, cuda_path, gencode_options, compileArch);
-    } catch (Exception ex) {
-      throw new RuntimeException("Failed to compile cuda code.", ex);
+            return new ParallelCompile().compile( generated, cuda_path,
+                                                  gencode_options, compileArch );
+        }
+        catch (Exception ex)
+        {
+            throw new RuntimeException( "Failed to compile cuda code.", ex );
+        }
     }
-  }
 
   @Override
   public String getDeviceFunctionQualifier() {
