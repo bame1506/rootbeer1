0096291d371cedb87adfc32cab086dfd818675be
mxmlnkn <mxmlnkn@github.de>
2016-07-19 03:32:50 +0200

    trying to modernize internals fo generated code, but now the loop doesn't run till the end anymore and not in all threads. This is actually similar to the error I'm searching in the first place for when I used Rootbeer with Spark on MonteCarloPi

--- src/org/trifort/rootbeer/generate/opencl/CudaHeader.c
+++ src/org/trifort/rootbeer/generate/opencl/CudaHeader.c
@@ -1,103 +1,52 @@
+
 #ifndef NAN
-#include <math_constants.h>
-#define NAN CUDART_NAN
+#   include <math_constants.h>
+#   define NAN CUDART_NAN
 #endif
 
 #ifndef INFINITY
-#include <math_constants.h>
-#define INFINITY CUDART_INF
+#   include <math_constants.h>
+#   define INFINITY CUDART_INF
 #endif
 
 #include <stdio.h>
+#include <assert.h>
+
 
 __constant__ size_t m_Local[3];
 __shared__ char m_shared[%%shared_mem_size%%];
 
-__device__
-int getThreadId(){
-  int blockSize = blockDim.x * blockDim.y * blockDim.z;
-  
-  int ret = (blockIdx.x * gridDim.y * blockSize) +
-            (blockIdx.y * blockSize) +
-            (threadIdx.x * blockDim.y * blockDim.z) +
-            (threadIdx.y * blockDim.z) +
-            (threadIdx.z);
-  return ret;
+
+__device__ int getThreadId()
+{
+    int linearized = 0;
+    int max        = 1;
+
+    // this ordering seems highly unorthodox to me.
+    // Normally threadIdx.x would be the least significant number
+    linearized += threadIdx.z * max; max *= blockDim.z;
+    linearized += threadIdx.y * max; max *= blockDim.y;
+    linearized += threadIdx.x * max; max *= blockDim.x;
+    linearized += blockIdx.y  * max; max *= gridDim.y;
+    linearized += blockIdx.x  * max;
+
+    return linearized;
 }
 
-__device__
-int getThreadIdxx(){
-  return threadIdx.x;
-}
-
-__device__
-int getThreadIdxy(){
-  return threadIdx.y;
-}
-
-__device__
-int getThreadIdxz(){
-  return threadIdx.z;
-}
-
-__device__
-int getBlockIdxx(){
-  return blockIdx.x;
-}
-
-__device__
-int getBlockIdxy(){
-  return blockIdx.y;
-}
-
-__device__
-int getBlockDimx(){
-  return blockDim.x;
-}
-
-__device__
-int getBlockDimy(){
-  return blockDim.y;
-}
-
-__device__
-int getBlockDimz(){
-  return blockDim.z;
-}
-
-__device__
-long long getGridDimx(){
-  return gridDim.x;
-}
-
-__device__
-long long getGridDimy(){
-  return gridDim.y;
-}
-
-__device__
-void org_trifort_syncthreads(){
-  __syncthreads();
-}
-
-__device__
-int org_trifort_syncthreads_count(int value){
-  return __syncthreads_count(value);
-}
-
-__device__
-void org_trifort_threadfence(){
-  __threadfence();
-}
-
-__device__
-void org_trifort_threadfence_block(){
-  __threadfence_block();
-}
-
-__device__
-void org_trifort_threadfence_system(){
-  __threadfence_system();
-}
+__device__ int getThreadIdxx(){ return threadIdx.x; }
+__device__ int getThreadIdxy(){ return threadIdx.y; }
+__device__ int getThreadIdxz(){ return threadIdx.z; }
+__device__ int getBlockIdxx (){ return blockIdx.x ; }
+__device__ int getBlockIdxy (){ return blockIdx.y ; }
+__device__ int getBlockDimx (){ return blockDim.x ; }
+__device__ int getBlockDimy (){ return blockDim.y ; }
+__device__ int getBlockDimz (){ return blockDim.z ; }
+__device__ long long getGridDimx(){ return gridDim.x; }
+__device__ long long getGridDimy(){ return gridDim.y; }
+__device__ void org_trifort_syncthreads(){ __syncthreads(); }
+__device__ int  org_trifort_syncthreads_count(int value){ return __syncthreads_count(value); }
+__device__ void org_trifort_threadfence(){ __threadfence(); }
+__device__ void org_trifort_threadfence_block(){ __threadfence_block(); }
+__device__ void org_trifort_threadfence_system(){ __threadfence_system(); }
 
 __device__ clock_t global_now;
--- src/org/trifort/rootbeer/generate/opencl/CudaKernel.c
+++ src/org/trifort/rootbeer/generate/opencl/CudaKernel.c
@@ -1,65 +1,124 @@
 
+// Note: MethodNameParser seems like it can't handle /**/ style comments ... -.-
+//
+// Caused by: java.lang.StringIndexOutOfBoundsException: String index out of range: -2
+// 	at java.lang.String.substring(String.java:1904)
+// 	at org.trifort.rootbeer.deadmethods.MethodNameParser.parseMethodName(MethodNameParser.java:55)
+// 	at org.trifort.rootbeer.deadmethods.MethodNameParser.parse(MethodNameParser.java:22)
+// 	at org.trifort.rootbeer.deadmethods.DeadMethods.parseString(DeadMethods.java:49)
+// 	at org.trifort.rootbeer.generate.opencl.tweaks.CudaTweaks.compileProgram(CudaTweaks.java:95)
+// 	... 9 more
+//
+// struct m_Local {
+//     unsigned long long dpObjectMem,        // 0
+//     unsigned long long objectMemSizeDiv16, // 1
+//     unsigned long long dpClassMem          // 2
+// }
+// @see CudaHeader.c csrc/org/trifort/rootbeer/runtime/CUDAContext.c
+
 __device__ int
-org_trifort_classConstant(int type_num){
-  int * temp = (int *) m_Local[2];   
-  return temp[type_num];
+org_trifort_classConstant( int type_num )
+{
+    int * temp = (int *) m_Local[2]; // dpClassMem
+    return temp[type_num];
 }
 
 __device__  char *
-org_trifort_gc_deref(int handle){
-
-  char * data_arr = (char * ) m_Local[0];
-  long long lhandle = handle;
-  lhandle = lhandle << 4;
-  return &data_arr[lhandle];
+org_trifort_gc_deref( int handle )
+{
+    char * data_arr = (char *) m_Local[0]; // dpObjectMem
+    long long lhandle = handle;
+    lhandle = lhandle << 4;
+    return &data_arr[lhandle];
 }
 
-__device__ int
-org_trifort_gc_malloc(int size){
-  int space_size = (int) m_Local[1];
-  int ret = org_trifort_gc_malloc_no_fail(size);
-  int end = ret + ((size + 16) >> 4);
-  if(end >= space_size){
-    return -1;
-  }
-  return ret;
-}
 
 //TODO: don't pass gc_info everywhere because free pointer is __device__
-__device__ int global_free_pointer; 
+//
+// This gets initialized to object_mem.get_heap_end() / 16
+// Meaning it is not directly a pointer, but a software "pointer" which
+// will be added to a real pointer.
+// The elements coming before it are the static data members of the kernel.
+// It is in units of 16 Bytes!
+//
+__device__ int global_free_pointer;
 
+int __device__ padNumberTo
+(
+    int const number,
+    int const multiplier
+)
+{
+    int const padded = number % multiplier == 0 ? number :
+                       number + multiplier - number % multiplier;
+    assert( padded % multiplier == 0 );
+    assert( padded >= number );
+    return padded;
+}
+
+//
+// Allocate at least the specified size in bytes. Align to 16 Bytes.
+//
+// gc means garbage collector.
+//
+// @return address for allocated memory chunk
+//
 __device__ int
-org_trifort_gc_malloc_no_fail(int size){
-  if(size % 16 != 0){
-    size += (16 - (size % 16));
-  }
-  size >>= 4;
+org_trifort_gc_malloc_no_fail( int size )
+{
+    int const lastAddress = atomicAdd( &global_free_pointer,
+                                       padNumberTo( size, 16 ) / 16 );
+    return lastAddress;
+}
 
-  int ret;
-  ret = atomicAdd(&global_free_pointer, size);
-  return ret;
+//
+// Identical to org_trifort_gc_malloc_no_fail, but checks if the returned
+// address is actually valid!
+//
+__device__ int
+org_trifort_gc_malloc( int size )
+{
+    int const & objectMemSizeDiv16 = (int) m_Local[1];
+    int const address = org_trifort_gc_malloc_no_fail( size );
+    // int const end = address + size/16 + 1; // this looks very wrong!
+    // if ( end >= objectMemSizeDiv16 )
+    //                           0 <= size <= 15 -> 1
+    // This is floor( size/16 ) + 1. It is stricter than this new version, so
+    // it shouldn't have led to a segfault, it was just confusing and lax */
+    if ( address + padNumberTo( size, 16 ) / 16 > objectMemSizeDiv16 )
+    {
+        assert( false && "Garbage collector ran out of memory!" );
+        // this needs to be checked by the caller! Else a segfault before the
+        // allocated memory might occur ... */
+        return -1;
+    }
+    return address;
 }
 
 __device__
 long long java_lang_System_nanoTime(int * exception){
-  return (long long) clock64();
+    return (long long) clock64();
 }
 
-__global__ void entry(int * handles, int * exceptions, int numThreads, 
-  int usingKernelTemplates){
-
-  int totalThreadId = getThreadId();
-  if(totalThreadId < numThreads){
-    int exception = 0; 
-    int handle;
-    if(usingKernelTemplates){
-      handle = handles[0];
-    } else {
-      handle = handles[totalThreadId];
+__global__ void entry
+(
+    int * handles   ,
+    int * exceptions,
+    int   numThreads,
+    int   usingKernelTemplates
+)
+{
+    int totalThreadId = getThreadId();
+    if ( totalThreadId < numThreads )
+    {
+        int exception = 0;
+        int handle;
+        if ( usingKernelTemplates )
+            handle = handles[0];
+        else
+            handle = handles[totalThreadId];
+        %%invoke_run%%(handle, &exception);
+        if ( %%using_exceptions%% )
+            exceptions[totalThreadId] = exception;
     }
-    %%invoke_run%%(handle, &exception);  
-    if(%%using_exceptions%%){
-      exceptions[totalThreadId] = exception;
-    }
-  }
 }
