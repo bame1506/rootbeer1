3bca40c7552ba54842cfc16b85440c6e42b74154
mxmlnkn <mxmlnkn@github.de>
2016-07-27 18:41:18 +0200

    aesthetics BclMemory

--- src/org/trifort/rootbeer/generate/bytecode/BclMemory.java
+++ src/org/trifort/rootbeer/generate/bytecode/BclMemory.java
@@ -7,32 +7,41 @@
 
 package org.trifort.rootbeer.generate.bytecode;
 
-/**
- * Imports needed:
- *   Local, Type, Value, LongType, IntType, VoidType, ByteType,
- *   BooleanType, ShortType, CharType, FloatType, DoubleType
- */
-import soot.*;
+
+import soot.Local      ;
+import soot.Type       ;
+import soot.Value      ;
+import soot.LongType   ;
+import soot.IntType    ;
+import soot.VoidType   ;
+import soot.ByteType   ;
+import soot.BooleanType;
+import soot.ShortType  ;
+import soot.CharType   ;
+import soot.FloatType  ;
+import soot.DoubleType ;
+
 import soot.jimple.IntConstant;
 import soot.jimple.LongConstant;
 
+
 /**
- * Seems to be just a wrapper to have access to Memory class from
- * analysed byte code. Also perfect case where Preprocessor could
- * reduce the code to 20%
+ * This class simplifies generating Jimple code for accesses to
+ * runtime/Memory.java class. Could basically be generated using preprocessors
+ * Uses BytecodeLanguage.pushMethod and .invokeMethodRet
  */
-public class BclMemory {
-  private final BytecodeLanguage mBcl;
-  private final Local mMem;
+public class BclMemory
+{
+    private final BytecodeLanguage mBcl;
+    private final Local mMem;
 
-  public BclMemory(BytecodeLanguage bcl, Local mem){
-    mBcl = bcl;
-    mMem = mem;
-  }
+    public BclMemory( BytecodeLanguage bcl, Local mem )
+    {
+        mBcl = bcl;
+        mMem = mem;
+    }
 
-  public void writeByte(byte value){
-    writeByte(IntConstant.v(value));
-  }
+    public void writeByte(byte value){ writeByte(IntConstant.v(value)); }
 
     /**
      * Same as @see callVoidMethod2, but takes takes no arguments
@@ -61,7 +70,7 @@ public class BclMemory {
     }
 
     /* why do they all have different scope specifiers ? (no specifier
-     * means package-private */
+     * means package-private. */
             void pushAddress       (){ voidCall0( "pushAddress"        ); }
             void popAddress        (){ voidCall0( "popAddress"         ); }
     public  void useInstancePointer(){ voidCall0( "useInstancePointer" ); }
@@ -70,6 +79,8 @@ public class BclMemory {
             void endIntegerList    (){ voidCall0( "endIntegerList"     ); }
             void finishReading     (){ voidCall0( "finishReading"      ); }
     public  void align             (){ voidCall0( "align"              ); }
+    /* In the end all these calls will finally after n indirections
+     * end up in their corresponding functions in FixedMemory.c */
     public  void writeByte       (Value v){ voidCall1( "writeByte   ",    ByteType.v(), v ); }
     public  void writeBoolean    (Value v){ voidCall1( "writeBoolean", BooleanType.v(), v ); }
             void writeShort      (Value v){ voidCall1( "writeShort  ",   ShortType.v(), v ); }
@@ -100,25 +111,17 @@ public class BclMemory {
     void writeInt(int size) { writeInt( IntConstant.v(size) ); }
     void incrementAddress(int size) { incrementAddress(IntConstant.v(size)); }
 
-    void writeVar(Local curr) {
-      Type type = curr.getType();
-      if(type instanceof ByteType){
-        writeByte(curr);
-      } else if(type instanceof BooleanType){
-        writeBoolean(curr);
-      } else if(type instanceof ShortType){
-        writeShort(curr);
-      } else if(type instanceof CharType){
-        writeChar(curr);
-      } else if(type instanceof IntType){
-        writeInt(curr);
-      } else if(type instanceof FloatType){
-        writeFloat(curr);
-      } else if(type instanceof DoubleType){
-        writeDouble(curr);
-      } else if(type instanceof LongType){
-        writeLong(curr);
-      }
+    void writeVar(Local curr)
+    {
+        final Type type = curr.getType();
+        if      ( type instanceof ByteType    ) writeByte   (curr);
+        else if ( type instanceof BooleanType ) writeBoolean(curr);
+        else if ( type instanceof ShortType   ) writeShort  (curr);
+        else if ( type instanceof CharType    ) writeChar   (curr);
+        else if ( type instanceof IntType     ) writeInt    (curr);
+        else if ( type instanceof FloatType   ) writeFloat  (curr);
+        else if ( type instanceof DoubleType  ) writeDouble (curr);
+        else if ( type instanceof LongType    ) writeLong   (curr);
     }
 
     private Local typedCall0
@@ -131,37 +134,29 @@ public class BclMemory {
         return mBcl.invokeMethodRet( mMem );
     }
 
-           Local readByte   (){ return typedCall0( "readByte"   ,    ByteType.v() ); }
-           Local readBoolean(){ return typedCall0( "readBoolean", BooleanType.v() ); }
-           Local readShort  (){ return typedCall0( "readShort"  ,   ShortType.v() ); }
-           Local readChar   (){ return typedCall0( "readChar"   ,    CharType.v() ); }
-           Local readInt    (){ return typedCall0( "readInt"    ,     IntType.v() ); }
-    public Local readRef    (){ return typedCall0( "readRef"    ,    LongType.v() ); }
-           Local readFloat  (){ return typedCall0( "readFloat"  ,   FloatType.v() ); }
-           Local readDouble (){ return typedCall0( "readDouble" ,  DoubleType.v() ); }
-           Local readLong   (){ return typedCall0( "readLong"   ,    LongType.v() ); }
-           Local getPointer (){ return typedCall0( "getPointer" ,    LongType.v() ); }
-           Local malloc     (){ return typedCall0( "malloc"     ,    LongType.v() ); }
+    Local readByte   (){ return typedCall0( "readByte"   ,    ByteType.v() ); }
+    Local readBoolean(){ return typedCall0( "readBoolean", BooleanType.v() ); }
+    Local readShort  (){ return typedCall0( "readShort"  ,   ShortType.v() ); }
+    Local readChar   (){ return typedCall0( "readChar"   ,    CharType.v() ); }
+    Local readInt    (){ return typedCall0( "readInt"    ,     IntType.v() ); }
+    Local readFloat  (){ return typedCall0( "readFloat"  ,   FloatType.v() ); }
+    Local readDouble (){ return typedCall0( "readDouble" ,  DoubleType.v() ); }
+    Local readLong   (){ return typedCall0( "readLong"   ,    LongType.v() ); }
+    Local getPointer (){ return typedCall0( "getPointer" ,    LongType.v() ); }
+    Local malloc     (){ return typedCall0( "malloc"     ,    LongType.v() ); }
+    public Local readRef(){ return typedCall0( "readRef", LongType.v() ); }
 
-    Local readVar(Type type) {
-      if(type instanceof ByteType){
-        return readByte();
-      } else if(type instanceof BooleanType){
-        return readBoolean();
-      } else if(type instanceof ShortType){
-        return readShort();
-      } else if(type instanceof CharType){
-        return readChar();
-      } else if(type instanceof IntType){
-        return readInt();
-      } else if(type instanceof FloatType){
-        return readFloat();
-      } else if(type instanceof DoubleType){
-        return readDouble();
-      } else if(type instanceof LongType){
-        return readLong();
-      }
-      throw new RuntimeException("How do we handle this case?");
+    Local readVar( final Type type )
+    {
+        if      ( type instanceof ByteType    ) return readByte   ();
+        else if ( type instanceof BooleanType ) return readBoolean();
+        else if ( type instanceof ShortType   ) return readShort  ();
+        else if ( type instanceof CharType    ) return readChar   ();
+        else if ( type instanceof IntType     ) return readInt    ();
+        else if ( type instanceof FloatType   ) return readFloat  ();
+        else if ( type instanceof DoubleType  ) return readDouble ();
+        else if ( type instanceof LongType    ) return readLong   ();
+        else throw new RuntimeException("How do we handle this case?");
     }
 
 }
