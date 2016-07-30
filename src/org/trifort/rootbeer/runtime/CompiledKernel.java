9eef30b969961b712bb20ebf21fcb5cb49739422
mxmlnkn <mxmlnkn@github.de>
2016-07-30 23:49:08 +0200

    aesthetics CompiledKernel

--- src/org/trifort/rootbeer/runtime/CompiledKernel.java
+++ src/org/trifort/rootbeer/runtime/CompiledKernel.java
@@ -7,18 +7,20 @@
 
 package org.trifort.rootbeer.runtime;
 
+
 public interface CompiledKernel
 {
-    public String     getCodeUnix();
-    public String     getCodeWindows();
-    public int        getNullPointerNumber();
-    public int        getOutOfMemoryNumber();
-    public String     getCubin32();
-    public int        getCubin32Size();
-    public boolean    getCubin32Error();
-    public String     getCubin64();
-    public int        getCubin64Size();
-    public boolean    getCubin64Error();
-    public Serializer getSerializer(Memory memory, Memory texture_memory); /* defined by Soot in VisitorGen.java? */
+    public String     getCodeUnix            ();
+    public String     getCodeWindows         ();
+    public int        getNullPointerNumber   ();
+    public int        getOutOfMemoryNumber   ();
+    public String     getCubin32             ();
+    public int        getCubin32Size         ();
+    public boolean    getCubin32Error        ();
+    public String     getCubin64             ();
+    public int        getCubin64Size         ();
+    public boolean    getCubin64Error        ();
     public boolean    isUsingGarbageCollector();
+    /* defined by Soot in VisitorGen.java? */
+    public Serializer getSerializer( Memory memory, Memory texture_memory );
 }
